84794655f77afe3a8aa530598be28179fe66552e
shahzaibali02 <alishahzaib02@gmail.com>
2024-11-19 17:02:37 +0500

    UPDATED HAPTIC ENGINE CODE

--- Haptic Engine/ContentView.swift
+++ Haptic Engine/ContentView.swift
@@ -2,11 +2,13 @@ import SwiftUI
 
 struct HapticTrackingView: View {
     @StateObject private var viewModel = HapticTrackingViewModel()
-
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 // Background
+                
+                
                 Color.clear
                     .contentShape(Rectangle())
                     .background(
@@ -14,35 +16,41 @@ struct HapticTrackingView: View {
                             .fill(viewModel.backgroundFlickerIntensity > 0.5 ? Color.green.opacity(viewModel.opacity) : Color.red.opacity(viewModel.opacity))
                             .edgesIgnoringSafeArea(.all)
                     )
-
                 
-                Circle()
-                    .fill(viewModel.randomDotModel.color)
-                    .frame(width: viewModel.randomDotModel.size, height: viewModel.randomDotModel.size)
-                    .position(viewModel.randomDotModel.position)
-
-                
-                Circle()
-                    .fill(viewModel.fingerDotModel.color)
-                    .frame(width: viewModel.fingerDotModel.size, height: viewModel.fingerDotModel.size)
+                Circle().stroke(Color.blue, lineWidth: 3) // Customize stroke color and width
+                    .scaleEffect(viewModel.rippleScale)
+                    .opacity(viewModel.rippleOpacity)
                     .position(viewModel.fingerDotModel.position)
-                    .opacity(viewModel.isDragging ? 1 : 0)
-            }
-            .gesture(
-                DragGesture(minimumDistance: 0)
-                    .onChanged { value in
-                        viewModel.onDragChanged(value, in: geometry)
+                    .frame(width: geometry.size.width, height:  geometry.size.height) // Adjust the maximum size of the ripple
+                  
+                        
+                        Circle()
+                            .fill(viewModel.randomDotModel.color)
+                            .frame(width: viewModel.randomDotModel.size, height: viewModel.randomDotModel.size)
+                            .position(viewModel.randomDotModel.position)
+                        
+                        
+                        Circle()
+                            .fill(viewModel.fingerDotModel.color)
+                            .frame(width: viewModel.fingerDotModel.size, height: viewModel.fingerDotModel.size)
+                            .position(viewModel.fingerDotModel.position)
+                            .opacity(viewModel.isDragging ? 1 : 0)
                     }
-                    .onEnded { _ in
-                        viewModel.onDragEnded()
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { value in
+                                viewModel.onDragChanged(value, in: geometry)
+                            }
+                            .onEnded { _ in
+                                viewModel.onDragEnded()
+                            }
+                    )
+                    .onAppear {
+                        viewModel.onAppear(in: geometry)
                     }
-            )
-            .onAppear {
-                viewModel.onAppear(in: geometry)
             }
         }
     }
-}
 
 struct HapticTrackingView_Previews: PreviewProvider {
     static var previews: some View {
--- Haptic Engine/HapticTrackingViewModel.swift
+++ Haptic Engine/HapticTrackingViewModel.swift
@@ -7,27 +7,130 @@
 
 import Foundation
 import UIKit
+import CoreHaptics
 import SwiftUI
+
 class HapticTrackingViewModel: ObservableObject {
 
+    
+    @Published  var rippleScale: CGFloat = 0.1
+     @Published  var rippleOpacity: Double = 0
+    
+    
     @Published private(set) var randomDotModel = DotModel(position: .zero, size: 0, color: .blue)
     @Published private(set) var fingerDotModel = DotModel(position: .zero, size: 0, color: .red)
     @Published private(set) var isDragging = false
     @Published private(set) var backgroundFlickerIntensity: CGFloat = 0
     @Published private(set) var opacity: CGFloat = 0
 
+    private var intensityParameter: CHHapticDynamicParameter?
+    private var continuousPlayer: CHHapticAdvancedPatternPlayer!
+    private var engine: CHHapticEngine!
     
-    private let impactFeedback = UIImpactFeedbackGenerator(style: .heavy)
-    private let selectionFeedback = UISelectionFeedbackGenerator()
+    
+
     private var MAX_DISTANCE : CGFloat = 0
+
+   
+    private let initialIntensity: Float = 1.0
+     private let initialSharpness: Float = 0.5
+    
+    func createAndStartHapticEngine() {
+          
+          // Create and configure a haptic engine.
+          do {
+              engine = try CHHapticEngine()
+          } catch let error {
+              fatalError("Engine Creation Error: \(error)")
+          }
+          
+          // Mute audio to reduce latency for collision haptics.
+          engine.playsHapticsOnly = true
+          
+          // The stopped handler alerts you of engine stoppage.
+          engine.stoppedHandler = { reason in
+              print("Stop Handler: The engine stopped for reason: \(reason.rawValue)")
+              switch reason {
+              case .audioSessionInterrupt:
+                  print("Audio session interrupt")
+              case .applicationSuspended:
+                  print("Application suspended")
+              case .idleTimeout:
+                  print("Idle timeout")
+              case .systemError:
+                  print("System error")
+              case .notifyWhenFinished:
+                  print("Playback finished")
+              case .gameControllerDisconnect:
+                  print("Controller disconnected.")
+              case .engineDestroyed:
+                  print("Engine destroyed.")
+              @unknown default:
+                  print("Unknown error")
+              }
+          }
+          
+          // The reset handler provides an opportunity to restart the engine.
+          engine.resetHandler = {
+              
+              print("Reset Handler: Restarting the engine.")
+              
+              do {
+                  // Try restarting the engine.
+                  try self.engine.start()
+                  
+                  // Indicate that the next time the app requires a haptic, the app doesn't need to call engine.start().
+//                  self.engineNeedsStart = false
+                  
+                  // Recreate the continuous player.
+                  self.createContinuousHapticPlayer()
+                  
+              } catch {
+                  print("Failed to start the engine")
+              }
+          }
+          
+          // Start the haptic engine for the first time.
+          do {
+              try self.engine.start()
+          } catch {
+              print("Failed to start the engine: \(error)")
+          }
+      }
+    func createContinuousHapticPlayer() {
+        // Create an intensity parameter:
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity,
+                                               value: initialIntensity)
+        
+        // Create a sharpness parameter:
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness,
+                                               value: initialSharpness)
+        
+        // Create a continuous event with a long duration from the parameters.
+        let continuousEvent = CHHapticEvent(eventType: .hapticContinuous,
+                                            parameters: [intensity, sharpness],
+                                            relativeTime: 0,
+                                            duration: 100)
+        
+        do {
+            // Create a pattern from the continuous haptic event.
+            let pattern = try CHHapticPattern(events: [continuousEvent], parameters: [])
+            
+            // Create a player from the continuous haptic pattern.
+            continuousPlayer = try engine.makeAdvancedPlayer(with: pattern)
+            
+            
+        } catch let error {
+            print("Pattern Player Creation Error: \(error)")
+        }
+        
+    }
     
     private func adaptiveSizes(for geometry: GeometryProxy) -> CGFloat {
         let screenSmallestDimension = min(geometry.size.width, geometry.size.height)
         
-        
         let baseDotSize = screenSmallestDimension * 0.20
     
-      
         MAX_DISTANCE = geometry.size.height
         return baseDotSize
     }
@@ -44,36 +147,102 @@ class HapticTrackingViewModel: ObservableObject {
         withAnimation(.easeInOut(duration: 0.3).repeatForever(autoreverses: true)) {
             randomDotModel.size = 100
         }
-        if hasFeedbackSupport {
-            impactFeedback.prepare()
-            selectionFeedback.prepare()
+        
+        // Setup haptic player when view appears
+        createAndStartHapticEngine()
+        createContinuousHapticPlayer()
+    }
+//    private var isContinuousHapticStarted = false
+    private var isHapticStarted = false
+    fileprivate func startContinuosPlayerEngine() {
+        do {
+            // Begin playing continuous pattern.
+            try continuousPlayer.start(atTime: CHHapticTimeImmediate)
+        } catch let error {
+            print("Error starting the continuous haptic player: \(error)")
         }
     }
+    let hapticDict = [
+            CHHapticPattern.Key.pattern: [
+                [CHHapticPattern.Key.event: [
+                    CHHapticPattern.Key.eventType: CHHapticEvent.EventType.hapticTransient,
+                    CHHapticPattern.Key.time: CHHapticTimeImmediate,
+                    CHHapticPattern.Key.eventParameters: [
+                        [CHHapticPattern.Key.parameterID: CHHapticEvent.ParameterID.hapticIntensity.rawValue,
+                         CHHapticPattern.Key.parameterValue: 1.0],
+                        [CHHapticPattern.Key.parameterID: CHHapticEvent.ParameterID.hapticSharpness.rawValue,
+                         CHHapticPattern.Key.parameterValue: 1]
+                    ],
+                    CHHapticPattern.Key.eventDuration: 0.2
+                ]]
+            ]
+        ]
 
+    
+    func singleTapHaptic(onCompleted : @escaping () -> Void) {
+        do {
+            let pattern = try CHHapticPattern(dictionary: hapticDict)
+            let player = try engine.makePlayer(with: pattern)
+            try engine.start()
+            try player.start(atTime: 0)
+            engine.notifyWhenPlayersFinished { error in
+                onCompleted()
+                return .leaveEngineRunning
+            }
+        }
+        catch{
+            onCompleted()
+            print("HAPTIC ENGINE ERROR")
+        }
+    }
     func onDragChanged(_ value: DragGesture.Value, in geometry: GeometryProxy) {
         isDragging = true
         fingerDotModel.position = constrainPosition(value.location, in: geometry.size, dotSize: fingerDotModel.size)
 
+        if !isHapticStarted {
+            isHapticStarted = true
+            singleTapHaptic{
+                if self.isDragging {
+                    self.startContinuosPlayerEngine()
+                }
+                
+            }
+           
+            withAnimation(.easeOut(duration: 0.6)) {
+                self.rippleScale = 1.0 // Ripple expands
+                self.rippleOpacity = 0.0 // Ripple fades out
+            } completion: {
+                self.rippleOpacity = 1
+                self.rippleScale = 0
+            }
+        }
         let distance = calculateDistance(from: fingerDotModel.position, to: randomDotModel.position)
         if distance < fingerDotModel.size {
-            if hasFeedbackSupport {
-                impactFeedback.impactOccurred(intensity: 1.0)
-            } else {
-                withAnimation(.easeInOut(duration: 0.1).repeatForever() ){
-                    opacity = opacity > 0 ? 0 : 1
-                }
-            }
             updateRandomDotModel(in: geometry.size, dotSize: randomDotModel.size)
-        } else if distance < MAX_DISTANCE {
-            let intensity = 1.0 - (distance / MAX_DISTANCE)
-            print("INTENSITY ",intensity)
-            backgroundFlickerIntensity = intensity
-            if hasFeedbackSupport {
-                selectionFeedback.prepare()
-                selectionFeedback.selectionChanged()
-                if distance < randomDotModel.size * 4 {
-                    impactFeedback.impactOccurred(intensity: intensity)
-                }
+        }
+        else if distance < MAX_DISTANCE {
+            let intensity : Float = Float(1.0 - (distance / MAX_DISTANCE))
+            print("INTENSITY ", intensity)
+            backgroundFlickerIntensity = CGFloat(intensity)
+            
+            if hasHepticEngineSupport {
+                // Create dynamic parameters for the updated intensity & sharpness.
+                           let intensityParameter = CHHapticDynamicParameter(parameterID: .hapticIntensityControl,
+                                                                             value: intensity,
+                                                                             relativeTime: 0)
+                           
+                           let sharpnessParameter = CHHapticDynamicParameter(parameterID: .hapticSharpnessControl,
+                                                                             value: intensity,
+                                                                             relativeTime: 0)
+                           
+                           // Send dynamic parameters to the haptic player.
+                           do {
+                               try continuousPlayer.sendParameters([intensityParameter, sharpnessParameter],
+                                                                   atTime: 0)
+                           } catch let error {
+                               print("Dynamic Parameter Error: \(error)")
+                           }
+                
             } else {
                 withAnimation(.easeInOut(duration: 0.1).repeatForever()) {
                     opacity = opacity > 0 ? 0 : 1
@@ -81,11 +250,35 @@ class HapticTrackingViewModel: ObservableObject {
             }
         } else {
             backgroundFlickerIntensity = 0
+            // Set intensity to zero when out of range
+//            if hasHepticEngineSupport {
+//                do {
+//                    intensityParameter = CHHapticDynamicParameter(
+//                        parameterID: .hapticIntensityControl,
+//                        value: 0,
+//                        relativeTime: 0
+//                    )
+//                    try hapticPlayer?.sendParameters([intensityParameter!],
+//                                                   atTime: CHHapticTimeImmediate)
+//                } catch {
+//                    print("Haptic update error: \(error)")
+//                }
+//            }
         }
     }
 
     func onDragEnded() {
+        isHapticStarted = false
         isDragging = false
+        cleanup() // Stop haptic feedback when drag ends
+    }
+    
+    func cleanup() {
+        do {
+                           try continuousPlayer.stop(atTime: CHHapticTimeImmediate)
+                       } catch let error {
+                           print("Error stopping the continuous haptic player: \(error)")
+                       }
     }
 
     private func calculateDistance(from point1: CGPoint, to point2: CGPoint) -> CGFloat {
@@ -103,7 +296,6 @@ class HapticTrackingViewModel: ObservableObject {
                                       size: dotSize,
                                       color: .blue)
         }
-       
     }
 
     private func constrainPosition(_ position: CGPoint, in size: CGSize, dotSize: CGFloat) -> CGPoint {
@@ -111,15 +303,9 @@ class HapticTrackingViewModel: ObservableObject {
         return CGPoint(x: min(max(position.x, halfDotSize), size.width - halfDotSize),
                        y: min(max(position.y, halfDotSize), size.height - halfDotSize))
     }
-
-    
-    private var hasFeedbackSupport: Bool {
-        if let majorVersion = Int(UIDevice.current.systemVersion.components(separatedBy: ".").first ?? "0") {
-            return majorVersion >= 13
-
-        } else {
-            return false
-        }
+  
+    private var hasHepticEngineSupport: Bool {
+        return CHHapticEngine.capabilitiesForHardware().supportsHaptics
     }
 
     private func randomPosition(in size: CGSize, dotSize: CGFloat) -> CGPoint {
